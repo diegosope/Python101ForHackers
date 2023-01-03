203d673cac6982ee19c9d293f5a0ecf8e320b6c5
diegosope <dsg.real.madrid@gmail.com>
2023-01-03 19:59:47 +0100

    -colors

--- sha256-crack.py
+++ sha256-crack.py
@@ -1,18 +1,6 @@
 from pwn import *
 import sys
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-
 if len(sys.argv) != 2:
 	print("Invalid arguments!")
 	print(">> {} <sha256sum>".format(sys.argv[0]))
